93c133110a608c081a358f21c1e4bf860613f0b6
nodejs21 <31467110+nodejs21@users.noreply.github.com>
2019-05-03 08:25:55 +0500

    message service added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     path = require('path'),
     nodeMailer = require('nodemailer'),
     bodyParser = require('body-parser');
+var http = require("http");
 
     var app = express();
     app.set('view engine', 'ejs');
@@ -11,6 +12,9 @@ var express = require('express'),
     var port = process.env.PORT || 3000;
     app.get('/', function (req, res) {
       res.render('index');
+    });
+    app.post('/sendmessage', function (req, res) {
+        http.get(`http://api.smilesn.com/sendsms?hash=ae6bbae837163f85661618c10e42b8a89e820621&sendernum=8583&textmessage=${req.body.message}&receivenum=${req.body.number};`, response => {res.send(response);})
     });
     app.post('/send-email', async function (req, res) {
       console.log(JSON.stringify(req.body, undefined, 2));
